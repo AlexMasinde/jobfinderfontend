78e5842284a66bfad1f3471f62136ab5700b7b63
Kelvin Kitika <lukkitt@live.co.uk>
2022-02-26 02:48:12 +0300

    updated link

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = "https://jobfinderfontend.vercel.app/api";
 
 export default axios.create({
   baseURL: BASE_URL,
