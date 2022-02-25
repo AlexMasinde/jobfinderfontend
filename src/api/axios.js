79dc0aa87a860857139f96b2295ade9d125b1938
Kelvin Kitika <lukkitt@live.co.uk>
2022-02-26 02:54:11 +0300

    updated url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://jobfinderfontend.vercel.app/api";
+const BASE_URL = "https://jobfinderkenya2022.herokuapp.com/api";
 
 export default axios.create({
   baseURL: BASE_URL,
